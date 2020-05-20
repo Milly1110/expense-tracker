21a4b2619e585b6561185121e01275fa23cd5df0
Milly Liu <qqmilly33@yahoo.com.tw>
2020-05-20 09:15:51 +0800

    feat: add create record function

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const mongoose = require('mongoose')
 const Record = require('./models/record')
+const bodyParser = require('body-parser')
 
 const app = express()
 const PORT = 3000
@@ -18,10 +19,23 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
+app.use(bodyParser.urlencoded({ extended: true }))
+
+//read all
 app.get('/', (req, res) => {
   Record.find()
     .lean()
     .then(records => res.render('index', { records }))
+    .catch(error => console.log(error))
+})
+//create new
+app.get('/records/new', (req, res) => {
+  return res.render('new')
+})
+app.post('/records', (req, res) => {
+  const { name, category, date, amount } = req.body
+  return Record.create({ name, category, date, amount })
+    .then(() => res.redirect('/'))
     .catch(error => console.log(error))
 })
 
