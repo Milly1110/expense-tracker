f6308494819857359417ab411989646ed2a9c73a
Milly Liu <qqmilly33@yahoo.com.tw>
2020-05-21 09:16:52 +0800

    feat: modify index page UI

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
-
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const Record = require('./models/record')
@@ -13,12 +12,10 @@ const PORT = 3000
 app.engine('hbs', exphbs({ defaulyLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
-
-
+app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.use(routes)
-
 
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`)
